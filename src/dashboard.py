038e2ebf1b97f2166a167729837424b89ed41b8c
Meznah Saldin <meznahsaldin@gmail.com>
2021-05-04 09:27:13 +1000

    Created config file and removed emoji

--- src/dashboard.py
+++ src/dashboard.py
@@ -9,13 +9,27 @@ from db.connector import db_instance
 import plotly.express as px
 import datetime
 from viz.nlp_summary import result1, result2
+import re
 #nltk.download('punkt') # needs to be run the first time the code is run on your machine, but can be commented out afterwards
+#### CHECK IF TOKEN IS EMOJI #####
+#$CWD/.streamlit/config.toml
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
 
 #### TEXT SUMMARISATION METHODS ####
 def unfiltered_frequency(messages):
     # takes in list of messages and returns a dataframe of words and their frequencies
     token_list = []
     for message in messages:
+        message = remove_emoji(message)
         message_tokens = nltk.tokenize.word_tokenize(message) #getting tokens from all chat messages
         for token in message_tokens:
             if token in stop_words: #takes out stop words
